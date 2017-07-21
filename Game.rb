ac6c794db71d93474389401ce5930931479a0976
William <william.chalons@gmail.com>
2017-07-21 13:41:24 +0200

    ajout de la methode winning

--- Game.rb
+++ Game.rb
@@ -25,46 +25,55 @@ class Game
   	answer = gets.chomp 
 
   	if answer == "1"
-	  	current_player = player_1
+      current_player = player_1
 	  	puts current_player.name
-	else
-		current_player = player_2
-		puts current_player.name
-	end
+    else
+		  current_player = player_2
+		  puts current_player.name
+    end
 
-	for n in 1..9 do
-	    if current_player == player_1
-	      play(current_player)
-	      current_player = player_2
-	    else
-	        play(current_player)
-	        current_player = player_1
-	    	end
-		end
-		@board.game_played
-		puts "			\n\n === MATCH NUL === 			"
-  	end
+  	for n in 1..9 do
+      if current_player == player_1
+        play(current_player)
+  	    current_player = player_2
+  	  else
+  	    play(current_player)
+  	    current_player = player_1
+      end
+      end
+  	
+      @board.game_played
+      puts " \n ================================= "
+      puts " =========== MATCH NUL =========== "
+      puts " ================================= "
+    end
   end
 
   def play(current_player)
   	@board.game_played
   	flag = true
-  	while flag do
-  		puts ""
-        print "Where do want to move? <1-9>: "
-        position = gets.chomp
+  	
+    while flag do
+      puts ""
+      print "Ou voulez-vous jouer? < 1-9 >: "
+      position = gets.chomp
 
-        if position.to_i < 1 || position.to_i > 9
-          puts "\nInvalid input, Please choose number between 1 to 9\n"
-          flag = true
-        elsif @board.positions[position] != " "
-          puts "\nPosition already occupied, Please choose another number...\n"
-          flag = true
-        else
-          flag = false
-          @board.positions[position] = current_player.mark
-        end
+      if position.to_i < 1 || position.to_i > 9
+        puts "\nImpossible de jouer ici. Veuillez choisir un numéro entre 1 et 9\n"
+        flag = true
+      elsif @board.positions[position] != " "
+        puts "\nCette case est déjà prise. Choisissez une autre case\n"
+        flag = true
+      else
+        flag = false
+        @board.positions[position] = current_player.mark
     end
+  end
+
+  def winning (board)
+
+  end
+
 end
 
 Game.new
